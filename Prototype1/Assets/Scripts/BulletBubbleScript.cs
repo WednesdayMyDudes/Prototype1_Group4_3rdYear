f4f0d063eff01f7be0e4f7b7f4b780300f1d8806
Mzizi <893943@students.wits.ac.za>
2019-07-25 17:44:41 +0200

    Shooting Scene -

---
+++ Prototype1/Assets/Scripts/BulletBubbleScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBubbleScript : MonoBehaviour
+{
+    public GameObject[] colorBubbles; // bubbles array
+
+    public BubbleScript.BUBBLE_TYPE typeOfBubbles; // bubble types enums 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Setting the next type of bubble before shooting 
+    public void SetType(BubbleScript.BUBBLE_TYPE typeOfBubbles)
+    {
+        
+        foreach (var go in colorBubbles)
+        {
+            go.SetActive(false);
+        }
+
+        this.typeOfBubbles = typeOfBubbles;
+
+        colorBubbles[(int)typeOfBubbles].SetActive(true); // Setting active the bubble that has been selected
+    }
+}
